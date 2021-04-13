b3955d49e1d1d6cf23e13f3372e2088357cedf12
lucasortizny <38075213+lucasortizny@users.noreply.github.com>
2021-04-13 17:41:33 -0400

    Use this to test logo rendering

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    
+    
+    
+    <head>
+        <title>TwitchPlaysPaperMario</title>
+        <meta charset="utf-8"/>
+    </head>
+
+    <body>
+        <div class="navigation-bar">
+            <img src="./images/smooth_2.png" alt="TPPM Logo" height="200px" width="auto"/>
+            <nav>
+            
+            </nav>
+        
+        
+        </div>
+    </body>
+
+
+</html>
