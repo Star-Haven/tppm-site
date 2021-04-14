c9fa2a11adaf368756a4291f940abebb77e2d2b3
lucasortizny <38075213+lucasortizny@users.noreply.github.com>
2021-04-13 23:21:29 -0400

    Added a generic navbar

--- index.php
+++ index.php
@@ -6,14 +6,18 @@
     <head>
         <title>TwitchPlaysPaperMario</title>
         <meta charset="utf-8"/>
+        <link href="./styles.css" rel="stylesheet" type="text/css"/>
     </head>
 
     <body>
-        <div class="navigation-bar">
-            <img src="./images/smooth_2.png" alt="TPPM Logo" height="200px" width="auto"/>
-            <nav>
+        <div class="navigation">
             
-            </nav>
+            <img class="tppmlogo" src="images/tppmlogo.svg" height="50px" width="auto"/>
+            <a class="navitem" id="trbotrelnav"><p class="navtext">TRBot</p></a>
+            <a class="navitem" id="twitchnav"><p class="navtext">Twitch</p></a>
+            <a class="navitem" id="currenthostnav"><p class="navtext">Current Host</p></a>
+            
+            
         
         
         </div>
