81f53b0f953285de0e1634ed4683ca23ddedce5c
lucasortizny <38075213+lucasortizny@users.noreply.github.com>
2021-04-14 01:26:35 -0400

    Have a generic navbar

--- index.php
+++ index.php
@@ -14,11 +14,12 @@
             
             <img class="tppmlogo" src="images/tppmlogo.svg" height="50px" width="auto"/>
             <a class="navitem" id="trbotrelnav"><p class="navtext">TRBot</p></a>
-            <a class="navitem" id="twitchnav"><p class="navtext">Twitch</p></a>
+            <a href="https://twitch.tv/twitchplayspapermario" class="navitem" id="twitchnav"><p class="navtext">Twitch</p></a>
             <a class="navitem" id="currenthostnav"><p class="navtext">Current Host</p></a>
             
             
-        
+        <!--- Find out a way to use PHP to ping the online status of the channel's
+        then render the html code as such --->
         
         </div>
     </body>
